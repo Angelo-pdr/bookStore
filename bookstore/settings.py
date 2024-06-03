9f85ce9e290d4472e6a1d359feea28e516ad3aa3
Angelo-pdr <angelo.ppedrosa@gmail.com>
2024-06-03 19:24:00 -0300

    deploy agora vai mesmo kk

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -73,9 +73,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "bookstore.wsgi.application"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -151,7 +151,7 @@ INTERNAL_IPS = [
 
 
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = False
 
 ALLOWED_HOSTS =  ["*","localhost", '127.0.0.1' "https://ebac-bookstore-ab40a816177e.herokuapp.com/", "/", "/favicon.ico" ]
 
