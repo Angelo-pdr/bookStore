2e1f4668ed2e2151b62f5e9329a91557fa1bfb2d
Angelo-pdr <angelo.ppedrosa@gmail.com>
2024-06-03 19:09:01 -0300

    deploy agora vai kk

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
@@ -8,3 +9,4 @@ urlpatterns = [
     re_path('bookstore/', include('product.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
